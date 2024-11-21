56bef30f5c493f80f0d842a250a8369aa93d9227
Riccardo Belliato <rbelliato@fbk.eu>
2024-11-21 10:13:17 +0100

    Update GRU4Rec class to include padding value and improve collate function; modify training function to use Adagrad optimizer and support regularization

--- src/gru4rec.py
+++ src/gru4rec.py
@@ -7,13 +7,16 @@ from torch.utils.data import Dataset, DataLoader
 from tqdm.auto import tqdm
 
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
 class SequentialRecommendations(Dataset):
-    def __init__(self, df: pd.DataFrame, n_negative_samples=10):
+    def __init__(self, df: pd.DataFrame, pad_value, n_negative_samples=10):
         super().__init__()
         self.positives = torch.from_numpy(df['ID'].values).view(-1, 1).to(DEVICE)        
         self.histories = [torch.from_numpy(h).to(DEVICE) for h in df['history']]
         self.negatives = [torch.tensor(n).to(DEVICE) for n in df['negatives']]
         self.n_negative_samples = n_negative_samples
+        self.pad_value = pad_value
     
     def __len__(self):
         return len(self.positives)
@@ -27,36 +30,34 @@ class SequentialRecommendations(Dataset):
 
         return history, pos, neg
 
-def collate_with_padding(batch):
-    histories, positives, negatives = zip(*batch)
-    padded_histories = torch.nn.utils.rnn.pad_sequence(histories, batch_first=True, padding_value=-1, padding_side='left')
-    return padded_histories, torch.stack(positives), torch.stack(negatives)
+    def collate_with_padding(self, batch):
+        histories, positives, negatives = zip(*batch)
+        padded_histories = torch.nn.utils.rnn.pad_sequence(histories, batch_first=True, padding_value=self.pad_value, padding_side='left')
+        return padded_histories, torch.stack(positives), torch.stack(negatives)
 
 
 class GRU4Rec(nn.Module):
-    def __init__(self, n_items, hidden_size=100, n_layers=1):
+    def __init__(self, n_items, hidden_size=100, n_layers=1, padding_idx=0):
         super().__init__()
         self.hidden_size = hidden_size
         self.n_layers = n_layers
+        self.padding_idx = padding_idx
 
-        self.embedding = nn.Embedding(n_items + 1, hidden_size, padding_idx=0)
+        self.embedding = nn.Embedding(n_items + 1, hidden_size, padding_idx=padding_idx)
         self.gru = nn.GRU(hidden_size + 1, hidden_size, n_layers, batch_first=True)
-        self.fc = nn.Linear(hidden_size, n_items)
+        self.fc = nn.Linear(hidden_size, n_items + 1)
 
     def forward(self, x: torch.Tensor):
-        items = x[:, :, 0] + 1 # 0 is padding
-        items = self.embedding(items)
-
-        outcomes = x[:, :, 1]
-        outcomes[outcomes == -1] = 0
-
-        x = torch.cat((items, outcomes.view(x.size(0), -1, 1)), dim=-1)
+        context = x[:, :, 1:]
+        context = context.masked_fill(context == self.padding_idx, 0)
+        x = torch.cat((self.embedding(x[:, :, 0]), context), dim=-1)
 
         h0 = torch.zeros(self.n_layers, x.size(0), self.hidden_size).to(DEVICE)
         out, _ = self.gru(x, h0)
-        out = out.mean(dim=1)
+        out = out[:, -1, :]
         out = self.fc(out)
         return out
+
 
 class BPRmax(nn.Module):
     def __init__(self, reg_lambda: float = 1.0, **kwargs):
@@ -70,12 +71,13 @@ class BPRmax(nn.Module):
         loss = -torch.log(F.sigmoid(sub) * negatives_weighted_softmax) + reg
         return loss.mean()
 
-def train(model: nn.Module, dataset: SequentialRecommendations, n_epochs=10, lr=0.01):
+
+def train(model: nn.Module, dataset: SequentialRecommendations, n_epochs=10, lr=0.01, reg_lambda=1.0):
     model.to(DEVICE)
     model.train()
-    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-    criterion = BPRmax()
-    dataloader = DataLoader(dataset, batch_size=32, shuffle=True, collate_fn=collate_with_padding)
+    optimizer = torch.optim.Adagrad(model.parameters(), lr=lr, lr_decay=1e-3)
+    criterion = BPRmax(reg_lambda=reg_lambda)
+    dataloader = DataLoader(dataset, batch_size=32, shuffle=True, collate_fn=dataset.collate_with_padding)
 
     for _ in (bar := tqdm(range(n_epochs))):
         epoch_loss = 0
