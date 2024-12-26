8cfbe4452bd57f5889b1d6115d98ea63b89ae63b
Riccardo Belliato <rbelliato@fbk.eu>
2024-12-26 10:43:47 +0000

    Add cumulative reward statistics and coverage metrics; refactor recommendation logic and evaluation functions

--- bandit.py
+++ bandit.py
@@ -1,7 +1,8 @@
 # %%
 import pandas as pd
+import numpy as np
 
-df = pd.read_csv('./data/October_November_missions_full.csv')
+df = pd.read_csv('./data/bq-results-20241223-153559-1734968168534.csv')
 df = df[~df['missionCatalog_id'].isin(['f1455712-fa2b-4feb-b7f9-ab8ddfa29e8d', 'd090d147-1ac9-4963-9c2b-1f5e663bad44'])]
 
 df.rename(columns={'sub': 'user', 'missionCatalog_id': 'missionID'}, inplace=True)
@@ -18,7 +19,7 @@ df['kind'] = df['kind'].astype('category')
 def reward(x):
     if x <= 1:
         return x
-    return max(0, 2 - x**2)
+    return max(0, 2 - np.pow(x, 2))
 
 df['reward'] = df['performance'].apply(reward)
 
@@ -30,9 +31,6 @@ n_users = df['user'].max() + 1
 n_missions = df['missionID'].max() + 1
 
 print(n_users, n_missions)
-
-# %% [markdown]
-# # MABTree
 
 # %%
 from src.tree import TreeNode
@@ -49,104 +47,69 @@ for name, round in missions.groupby('kind', observed=True):
 print(root)
 
 missions = missions.set_index('missionID')
+print(missions)
 
 # %%
 from src import policy as pol
 from src.tree import TreeBandit
 from tqdm.auto import tqdm
+ 
+class MABRecommender:
+    def __init__(self, policy: pol.Policy):
+        self.policy = policy
 
-def recommend(rank: list, missions: pd.DataFrame, n=1) -> list:
-    ranked_missions = missions.loc[rank] # ranks missions
-    top_missions = ranked_missions.groupby('kind', observed=True, sort=False).head(1) # selects the top mission of each kind
-    return top_missions.index.tolist()[:n] # returns the top n missions
+    def recommend(self):
+        pass
+
+class MABTreeRecommender(MABRecommender):
+    def __init__(self, policy: pol.Policy, root: TreeNode):
+        super().__init__(policy)
+        self.root = root
+        self.tree_bandit = TreeBandit(self.root, self.policy)
+    
+    def recommend(self, user, n=(3, 1)):
+        recs = self.tree_bandit.select(n=n, user=user)
+        return [m.value['missionID'] for m in recs]
+
+class MABPolicyRecommender(MABRecommender):
+    def __init__(self, policy: pol.Policy, missions: pd.DataFrame):
+        super().__init__(policy)
+        self.missions = missions
+    
+    def recommend(self, user, n=3):
+        rank = self.policy.select(user)
+        ranked_missions = self.missions.loc[rank] # ranks missions
+        top_missions = ranked_missions.groupby('kind', observed=True, sort=False).head(1) # selects the top mission of each kind
+        return top_missions.index.tolist()[:n] # returns the top n missions
 
 
-def replay_mabtree(df: pd.DataFrame, policy: pol.Policy, **kwargs):
-    root = kwargs.get('root', None)
-
+def replay(df: pd.DataFrame, recommeder: MABRecommender):
     history = pd.DataFrame()
-    all_recs = []
-    tree_bandit = TreeBandit(root, policy)
+    policy_recommendations = []
 
-    policy.init(reset_model=True) # resets the policy
+    recommeder.policy.init()
     for t, round in tqdm(df.groupby('createdAt'), leave=False):
         day_recs = []
         for u in tqdm(round['user'].unique(), leave=False):
-            recs = [{'user': u, 'missionID': m.value['missionID']} for m in tree_bandit.select(n = (3, 1), user=u)]
-            day_recs += recs
-
-        all_recs += day_recs
+            user_recs = [{'user': u, 'missionID': m} for m in recommeder.recommend(u)]
+            day_recs += user_recs
+        
+        policy_recommendations += day_recs
         actions = round.merge(pd.DataFrame(day_recs), on=['user', 'missionID'], how='inner')
         history = pd.concat((history, actions), ignore_index=True)
-        policy.update(train_df=history, day=t)
-            
-    return history, all_recs
-
-
-def replay(df: pd.DataFrame, policy: pol.Policy, **kwargs):
-    missions = kwargs.get('missions', None)
-
-    history = pd.DataFrame()
-    all_recs = []
-    policy.init(reset_model=True) # resets the policy
-    for t, round in tqdm(df.groupby('createdAt'), leave=False):
-        day_recs = []
-        for u in tqdm(round['user'].unique(), leave=False):
-            rank = policy.select(u)
-            recs = [{'user': u, 'missionID': rec} for rec in recommend(rank, missions, n=3)]
-            day_recs += recs
-
-        all_recs += day_recs
-        actions = round.merge(pd.DataFrame(day_recs), on=['user', 'missionID'], how='inner')
-        history = pd.concat((history, actions), ignore_index=True)
-        policy.update(train_df=history, day=t)
-            
-    return history, all_recs
+        recommeder.policy.update(train_df=history, day=t)
+    
+    return history, pd.DataFrame(policy_recommendations)
 
 # %%
-from collections import Counter
-import numpy as np
+def avg_coverage(df: pd.DataFrame):
+    return df.groupby('user')['missionID'].nunique().apply(lambda x: x / n_missions).mean()
 
-def calculate_entropy(data):
-    counter = Counter(data)
-    n = len(data)
-    return - sum([count/n * np.log2(count/n) for count in counter.values()])
+def evaluate(recommender: MABRecommender):
+    history, policy_recommendations = replay(df[['user', 'missionID', 'createdAt', 'reward']], recommender)
 
-
-def calculate_coverage(data):
-    return len(set(data)) / n_missions
-
-
-def evaluate(policy, replay, **kwargs) -> tuple[pd.DataFrame, float]:
-    rewards, recs = replay(df[['user', 'missionID', 'createdAt', 'reward']], policy, **kwargs)
-
-    recs = pd.DataFrame(recs)
-    entropy = recs.groupby('user')['missionID'].agg(calculate_entropy).mean()
-    coverage = recs.groupby('user')['missionID'].agg(calculate_coverage).mean()
-    rewards = rewards.groupby('createdAt')['reward'].sum().cumsum()
-
-    return rewards, entropy, coverage
-
-
-def repeated_evaluation(policies: dict, replay_fn, n=10, **kwargs) -> list[pd.DataFrame]:
-    metrics = [[], [], []]
-
-    for _ in tqdm(range(n)):
-        round_rewards = dict()
-        round_entropies = dict()
-        round_coverages = dict()
-        for name, policy in tqdm(policies.items(), leave=False):
-            round_rewards[name], round_entropies[name], round_coverages[name] = evaluate(policy, replay_fn, **kwargs)
-    
-        metrics[0].append(pd.concat(round_rewards))
-        metrics[1].append(round_entropies)
-        metrics[2].append(round_coverages)
-
-    metrics[0] = pd.concat(metrics[0], axis=1)
-    metrics[1] = pd.DataFrame(metrics[1])
-    metrics[2] = pd.DataFrame(metrics[2])
-
-    return metrics
+    cumulated_reward = history.groupby('createdAt')['reward'].sum().cumsum()
+    return cumulated_reward, avg_coverage(policy_recommendations)
 
 # %%
 import torch
@@ -158,34 +121,36 @@ torch.manual_seed(0)
 numpy.random.seed(0)
 
 policies = {
-    'MABTree \u03B5-greedy MF': pol.MABTreeEpsilonGreedyML(model_class=mod.MF, num_users=n_users, num_missions=n_missions, embedding_dim=8),
-    'MABTree \u03B5-greedy': pol.MABTreeEpsilonGreedy(),
-    'MABTree UCB1': pol.MABTreeUCB(),
-    'MABTree Random': pol.MABTReeRandom(),
+    'MABTree Random': MABTreeRecommender(pol.MABTReeRandom(), root),
+    'MABTree UCB': MABTreeRecommender(pol.MABTreeUCB(), root),
+    'MABTree 0.1-greedy': MABTreeRecommender(pol.MABTreeEpsilonGreedy(0.1), root),
+    'MABTree 0.1-greedy MF': MABTreeRecommender(pol.MABTreeEpsilonGreedyML(model_class=mod.MF, num_users=n_users, num_missions=n_missions, embedding_dim=8), root),
+    '0.1-greedy': MABPolicyRecommender(base.EpsilonGreedy(num_arms=n_missions, epsilon=0.1), missions),
+    'UCB': MABPolicyRecommender(base.UCB1(num_arms=n_missions), missions),
+    'LinUCB': MABPolicyRecommender(base.LinUCB(num_arms=n_missions, context_manager=base.ContextManager(n_users, n_missions)), missions),
 }
-
-metrics = repeated_evaluation(policies, replay_mabtree, root=root)
-metrics[0].to_csv('./out/mabtree_rewards.csv', index=True)
-metrics[1].to_csv('./out/mabtree_entropies.csv', index=True)
-metrics[2].to_csv('./out/mabtree_coverages.csv', index=True)
 
 # %%
-import torch
-import numpy
-from src import baseline as base
+metrics = [
+    {name: evaluate(recommender) for name, recommender in tqdm(policies.items(), leave=False)}
+    for _ in tqdm(range(10))
+]
 
-torch.manual_seed(0)
-numpy.random.seed(0)
+# %%
+# Cumulated reward dataframe
 
-policies = {
-    'LinUCB': base.LinUCB(num_arms=n_missions, context_manager=base.ContextManager(n_users=n_users, n_features=n_missions)),
-    'UCB1': base.UCB1(num_arms=n_missions),
-    '\u03B5-greedy': base.EpsilonGreedy(num_arms=n_missions),
-}
+pd.concat([
+    pd.concat({
+        name: reward for name, (reward, _) in metric.items()
+    }) for metric in metrics
+], axis=1).to_csv('./out/cumulated_reward.csv', index=True)
 
-metrics = repeated_evaluation(policies, replay, missions=missions)
-metrics[0].to_csv('./out/baseline_rewards.csv', index=True)
-metrics[1].to_csv('./out/baseline_entropies.csv', index=True)
-metrics[2].to_csv('./out/baseline_coverages.csv', index=True)
+# %%
+# Coverage dataframe
+
+pd.DataFrame([
+    {name: coverage for name, (_, coverage) in metric.items()}
+    for metric in metrics
+]).to_csv('./out/coverage.csv', index=True)
 
 
